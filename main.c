395878caa01fadec550c9303f14def88865734ad
page2me kitarotao <page2me@msn.com>
2020-06-06 20:54:23 +0000

    Write a function that prints a table of values for sine and cosine between (0, 1)

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include<math.h> /* has  sin(), abs(), and fabs() */
 int main(void)
 { 
-  double x;
-//double interval;
-//int i;
-//for(i = 0; i <30; i++)
-//{
-// interval = i/10.0;
-  printf("input x:");
-  scanf("%lf",&x);
-  printf("sin( %lf ) = %lf \n", x, fabs(sin(x)));
-//}
+// double x;
+double interval;
+int i;
+for(i = 0; i <11; i++)
+{ 
+ interval = i/10.0;
+  //printf("input x:");
+  //scanf("%lf",&x);
+  printf("sin( %lf ) = %lf \n", interval, fabs(sin(interval)));
+  printf("cos( %lf ) = %lf \n", interval, fabs(cos(interval)));
+}
 
 
 //printf("\n+++++++\n");
