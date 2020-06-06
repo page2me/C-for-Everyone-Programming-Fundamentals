482aa28720e62471cbec859ce0c20999efc09e9e
page2me kitarotao <page2me@msn.com>
2020-06-06 20:38:18 +0000

    Write a program that prints the sine function for an input x between (0, 1)

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include<math.h> /* has  sin(), abs(), and fabs() */
 int main(void)
 { 
-double interval;
-int i;
-for(i = 0; i <30; i++)
-{
- interval = i/10.0;
- printf("sin( %lf ) = %lf \t", interval, fabs(sin(interval)));
-}
+  double x;
+//double interval;
+//int i;
+//for(i = 0; i <30; i++)
+//{
+// interval = i/10.0;
+  printf("input x:");
+  scanf("%lf",&x);
+  printf("sin( %lf ) = %lf \n", x, fabs(sin(x)));
+//}
 
 
-printf("\n+++++++\n");
+//printf("\n+++++++\n");
 return 0;
 }
